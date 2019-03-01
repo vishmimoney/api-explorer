b20de66f22680e949a680ba7061d19f2d9c8724b
vishmimoney <vishmimoney@gmail.com>
2019-03-01 20:54:12 +0100

    Add initial project files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './components/Header/index.js';
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
+class App extends Component {
+  render() {
+    return (
+      <Header></Header>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h2>API explorer</h2>
+            </div>
+        );
+    }
+}
+
+export default Header;
